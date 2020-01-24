c46aafe1f5348d3f244db815b81d7bfa49b684f8
Great-Keith <3160104229@zju.edu.cn>
2020-01-24 12:20:48 +0800

    refact

---
+++ include/type_utils.h
@@ -0,0 +1,20 @@
+#pragma once
+template <typename T>
+struct is_double {
+	operator bool() {
+		return false;
+	}
+};
+
+template <>
+struct is_double<double> {
+	operator bool() {
+		return true;
+	}
+};
+
+
+template<class T>
+int length(T& arr) {
+	return sizeof(arr) / sizeof(arr[0]);
+}
